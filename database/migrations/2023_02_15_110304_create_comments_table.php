d288c652615f17b062704d5e1de3b65065468d7b
alizeinodin <alizeinodin79@gmail.com>
2023-02-15 15:00:04 +0330

    alter table add in comments table

--- database/migrations/2023_02_15_110304_create_comments_table.php
+++ database/migrations/2023_02_15_110304_create_comments_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -15,7 +16,7 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->comment('');
-            $table->integer('id', true);
+            $table->id();
             $table->integer('post_id')->index('FK_CPostId');
             $table->integer('user_id')->index('FK_CUserId');
             $table->enum('status', ['0', '1', '2'])->default('0')->comment('#0 : pending | #1 : ejected | #2 : published');
@@ -26,8 +27,16 @@ return new class extends Migration
             $table->timestamp('time')->useCurrent();
 
             $table->index(['comment_id', 'comment_post_id', 'comment_user_id'], 'Reply');
-            $table->primary(['id', 'post_id', 'user_id']);
         });
+
+        /*
+         * Blueprint not support two index in
+         * primary key, so we define this
+         * property, in unprepared DB
+         */
+
+        DB::unprepared("ALTER TABLE `wooden_door`.`comments` DROP PRIMARY KEY, ADD PRIMARY KEY (  `id` , `post_id`, `user_id` )");
+
     }
 
     /**
