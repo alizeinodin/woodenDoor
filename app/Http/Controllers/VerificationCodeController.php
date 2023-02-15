b816613c8c3be793f6c35f4fde091dd29c01cf97
alizeinodin <alizeinodin79@gmail.com>
2023-02-16 01:39:58 +0330

    Add send method in VerificationCodeController

--- app/Http/Controllers/VerificationCodeController.php
+++ app/Http/Controllers/VerificationCodeController.php
@@ -2,19 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\VerifiactionCode\SendRequest;
 use App\Mail\VerificationCodeMail;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\Routing\ResponseFactory;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Mail;
+use Symfony\Component\HttpFoundation\Response as ResponseHttp;
 
 class VerificationCodeController extends Controller
 {
     /**
+     * send verification code to email of user
+     *
      * @throws \Exception
      */
-    public function send()
+    public function send(SendRequest $request): Response|Application|ResponseFactory
     {
+        $cleanData = $request->validated();
+
         $code = random_int(100000, 999999);
 
-        Mail::to();
+        Mail::to($cleanData['email'])->send(new VerificationCodeMail($code));
+
+        $response = [
+            'message' => 'verification code sent'
+        ];
+
+        return response($response, ResponseHttp::HTTP_OK);
     }
 }
