b7ce7ecd64786fd66ef383e0e75d93a8d0b0aaae
alizeinodin <alizeinodin79@gmail.com>
2023-02-17 00:00:59 +0330

    Develop test user register

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature;
 
+use App\Enum\VerificationCodeStatus;
+use App\Models\VerificationCode;
 use Illuminate\Foundation\Testing\LazilyRefreshDatabase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -16,9 +18,32 @@ class AuthTest extends TestCase
      */
     public function test_user_register(): void
     {
+
+        $email = 'alizeinodin79@gmail.com';
+
+        $request = [
+            'email' => $email
+        ];
+
+        $response = $this->postJson(route('api.verification_code.send', $request));
+
+        $response->assertOk();
+
+        $code = VerificationCode::where('email', $email)->latest('id')->first()['code'];
+
+        $request['code'] = $code;
+
+        $response = $this->postJson(route('api.verification_code.verify', $request));
+
+        $response->assertOk();
+
+        $verifiable = VerificationCode::where('email', $email)->latest('id')->first();
+
+        $this->assertEquals(VerificationCodeStatus::confirmed, $verifiable['verify']);
+
         $request = [
             'username' => 'test',
-            'email' => 'email@gmail.com',
+            'email' => $email,
             'password' => 'password',
             'first_name' => 'name',
             'last_name' => 'last_name',
