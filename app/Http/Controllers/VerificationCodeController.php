468af8a9c00514e7ab96e48e4e5c7877bd91e109
alizeinodin <alizeinodin79@gmail.com>
2023-02-16 12:35:24 +0330

    Add verificationCode Model in send Method

--- app/Http/Controllers/VerificationCodeController.php
+++ app/Http/Controllers/VerificationCodeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\VerifiactionCode\SendRequest;
 use App\Mail\VerificationCodeMail;
+use App\Models\VerificationCode;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\Routing\ResponseFactory;
 use Illuminate\Http\Request;
@@ -23,6 +24,11 @@ class VerificationCodeController extends Controller
         $cleanData = $request->validated();
 
         $code = random_int(100000, 999999);
+
+        VerificationCode::create([
+            'email' => $cleanData['email'],
+            'code' => $code
+        ]);
 
         Mail::to($cleanData['email'])->send(new VerificationCodeMail($code));
 
