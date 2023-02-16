b7df640c754d43b9cf2e30dc8dfd2bc6eaf230c1
alizeinodin <alizeinodin79@gmail.com>
2023-02-16 22:50:28 +0330

    Add EnsureEmailIsVerified middleware

---
+++ app/Http/Middleware/Auth/EnsureEmailIsVerified.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Middleware\Auth;
+
+use App\Http\Requests\VerificationCode\SendRequest;
+use App\Models\VerificationCode;
+use Closure;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\Routing\ResponseFactory;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Symfony\Component\HttpFoundation\Response as ResponseHttp;
+
+class EnsureEmailIsVerified
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param SendRequest $request
+     * @param \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @return Application|ResponseFactory|Response
+     */
+    public function handle(SendRequest $request, Closure $next): Application|ResponseFactory|Response
+    {
+        $cleanData = $request->validated();
+
+        $verifiable = VerificationCode::where('email', $cleanData['email'])->notExpired()->latest('id')->first();
+
+        if (is_null($verifiable)) {
+            $response = [
+                'message' => 'First, verify your email',
+            ];
+
+            return response($response, ResponseHttp::HTTP_FORBIDDEN);
+        }
+        return $next($request);
+    }
+}
