0a51d5ea6a21fd75c447c024cf44e3fb54d7736e
alizeinodin <alizeinodin79@gmail.com>
2023-02-16 14:57:36 +0330

    Add test verify email by verification code

--- tests/Feature/VerificationCodeTest.php
+++ tests/Feature/VerificationCodeTest.php
@@ -2,6 +2,9 @@
 
 namespace Tests\Feature;
 
+use App\Enum\VerificationCodeStatus;
+use App\Models\VerificationCode;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\LazilyRefreshDatabase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -10,7 +13,6 @@ use Tests\TestCase;
 class VerificationCodeTest extends TestCase
 {
     use LazilyRefreshDatabase;
-
     /**
      * Test send verification
      * code to email of user
@@ -24,5 +26,34 @@ class VerificationCodeTest extends TestCase
         $response = $this->postJson(route('api.verification_code.send', $request));
 
         $response->assertOk();
+    }
+
+    /**
+     * Test verify email
+     * by check code
+     */
+    public function test_verify_email_by_verification_code()
+    {
+        $email = 'alizeinodin79@gmail.com';
+
+        $request = [
+            'email' => $email
+        ];
+
+        $response = $this->postJson(route('api.verification_code.send', $request));
+
+        $response->assertOk();
+
+        $code = VerificationCode::where('email', $email)->latest('id')->first()['code'];
+
+        $request['code'] = $code;
+
+        $response = $this->postJson(route('api.verification_code.verify', $request));
+
+        $response->assertOk();
+
+        $verifiable = VerificationCode::where('email', $email)->latest('id')->first();
+
+        $this->assertEquals(VerificationCodeStatus::confirmed, $verifiable['verify']);
     }
 }
