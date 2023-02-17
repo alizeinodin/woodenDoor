2288470a444d8b477800ca4ef4696ab5b5882894
alizeinodin <alizeinodin79@gmail.com>
2023-02-17 15:23:52 +0330

    Add test for login as user

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -53,4 +53,60 @@ class AuthTest extends TestCase
 
         $response->assertCreated();
     }
+
+    public function test_login_user()
+    {
+        /*
+         * register a user with
+         * email and verify
+         */
+        // --------------------------------------------
+        $email = 'alizeinodin79@gmail.com';
+        $password = 'password';
+
+        $request = [
+            'email' => $email
+        ];
+
+        $response = $this->postJson(route('api.verification_code.send', $request));
+
+        $response->assertOk();
+
+        $code = VerificationCode::where('email', $email)->latest('id')->first()['code'];
+
+        $request['code'] = $code;
+
+        $response = $this->postJson(route('api.verification_code.verify', $request));
+
+        $response->assertOk();
+
+        $verifiable = VerificationCode::where('email', $email)->latest('id')->first();
+
+        $this->assertEquals(VerificationCodeStatus::confirmed, $verifiable['verify']);
+
+        $request = [
+            'username' => 'test',
+            'email' => $email,
+            'password' => $password,
+            'first_name' => 'name',
+            'last_name' => 'last_name',
+            'sex' => 'MALE'
+        ];
+        $response = $this->postJson(route('api.auth.register'), $request);
+
+        $response->assertCreated();
+
+        // --------------------------------------------
+
+        $request = [
+            'email' => $email,
+            'password' => $password,
+        ];
+
+        $response = $this->postJson(route('api.auth.login', $request));
+
+        $response->assertOk();
+
+        $this->assertAuthenticated();
+    }
 }
