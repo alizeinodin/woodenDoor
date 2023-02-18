856ae6baab9a0a75dc7138551779779b6a91d4b6
alizeinodin <alizeinodin79@gmail.com>
2023-02-18 17:23:31 +0330

    Add fillable array in employee

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -6,9 +6,22 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * @method static create()
+ */
 class Employee extends Model
 {
     protected $table = 'employees';
+
+    protected $fillable = [
+        'province',
+        'address',
+        'about_me',
+        'min_salary',
+        'military_status',
+        'job_position_title',
+        'job_position_status',
+    ];
 
     /**
      * Get the user that owns the phone
