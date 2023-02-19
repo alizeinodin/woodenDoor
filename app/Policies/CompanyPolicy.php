080ad92dd1d8c1d07935fecef8f75b6631273cfc
alizeinodin <alizeinodin79@gmail.com>
2023-02-19 16:31:32 +0330

    Add update method in CompanyPolicy

--- app/Policies/CompanyPolicy.php
+++ app/Policies/CompanyPolicy.php
@@ -3,11 +3,21 @@
 namespace App\Policies;
 
 use App\Http\Requests\Company\StoreRequest;
+use App\Models\Company;
+use Illuminate\Http\Request;
 
 class CompanyPolicy
 {
     public function create(StoreRequest $request): bool
     {
         return $request->user()->role == 'Employer';
+    }
+
+    public function update(Request $request, Company $company): bool
+    {
+        return
+            ($request->user()->role == 'Employer')
+            and
+            (Company::where('employer_id', $request->user()->employer() == $company->employer()));
     }
 }
