38d91f4f99a2d5e7131e7157402efc846b88dd62
alizeinodin <alizeinodin79@gmail.com>
2023-02-19 18:36:57 +0330

    Add test get employers company method

--- tests/Feature/CompanyTest.php
+++ tests/Feature/CompanyTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature;
 
+use App\Models\Company;
+use App\Models\Employer;
 use App\Models\User;
 use Laravel\Sanctum\Sanctum;
 use Tests\TestCase;
@@ -20,6 +22,29 @@ class CompanyTest extends TestCase
             User::factory()->create()
         );
         $response = $this->getJson(route('api.company.index'));
+        $response->assertOk();
+    }
+
+    public function test_get_employers_company()
+    {
+        $user = User::factory()->create();
+
+        $employer = new Employer();
+        $employer->score = 10;
+
+        $employer->user()->associate($user);
+        $employer->save();
+
+        $company = new Company();
+        $company->persian_name = 'test1';
+        $company->english_name = 'test2';
+        $company->nick_name = 'test';
+
+        $user->employer->companies()->save($company);
+
+        Sanctum::actingAs($user);
+
+        $response = $this->postJson(route('api.company.my_companies'));
         $response->assertOk();
     }
 }
