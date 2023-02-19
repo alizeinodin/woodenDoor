547a30b3c2fdf79763e4069ba2ee3a5db083c31b
alizeinodin <alizeinodin79@gmail.com>
2023-02-19 09:56:06 +0330

    Add one to many relationship for employer and company

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Company extends Model
 {
     use HasFactory;
+
+    public function employer(): BelongsTo
+    {
+        return $this->belongsTo(Employer::class, 'employer_id');
+    }
 }
--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Employer extends Model
 {
     protected $table = 'employers';
+
+    public function companies(): HasMany
+    {
+        return $this->hasMany(Company::class);
+    }
 
     use HasFactory;
 }
