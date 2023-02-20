25b04d9062180cb49ec394bbebeee072847c238d
alizeinodin <alizeinodin79@gmail.com>
2023-02-20 18:48:21 +0330

    Add HasRoleForLogin middleware

---
+++ app/Http/Middleware/Auth/HasRoleForLogin.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Middleware\Auth;
+
+use App\Models\User;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpFoundation\Response as ResponseHttp;
+
+class HasRoleForLogin
+{
+    private const EMPLOYEE_ROLE = 'Employee';
+    private const EMPLOYER_ROLE = 'Employer';
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     * @param \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @return Response
+     * @throws ValidationException
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required|string',
+            'type' => 'required|bool',
+        ]);
+
+        if ($validator->fails()) {
+            throw ValidationException::withMessages((array)$validator->errors());
+        }
+        $email = $request->input('email');
+        $type = $request->input('type');
+
+        $user = User::where('email', $email)->latest('id')->first();
+
+        $result = $type ? is_null($user->employee) : is_null($user->employer);
+
+        if ($result) {
+
+            $response = [
+                'message' => 'You are not registered'
+            ];
+
+            return response($response, ResponseHttp::HTTP_UNAUTHORIZED);
+        }
+
+
+        return $next($request);
+    }
+}
