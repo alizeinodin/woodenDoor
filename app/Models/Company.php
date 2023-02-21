bd79695149ed342976c95c0fa25a564fee893bef
alizeinodin <alizeinodin79@gmail.com>
2023-02-21 22:42:54 +0330

    Add one to many relationship

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Company extends Model
 {
@@ -26,5 +27,10 @@ class Company extends Model
     public function employer(): BelongsTo
     {
         return $this->belongsTo(Employer::class, 'employer_id', 'user_id');
+    }
+
+    public function jobads(): HasMany
+    {
+        return $this->hasMany(JobAd::class, 'company_id', 'id');
     }
 }
