8fdeef2e1a34fe40a7b26e75ecb3773d49895f8d
alizeinodin <alizeinodin79@gmail.com>
2023-02-22 16:22:47 +0330

    Add delete method

--- app/Policies/JobAdPolicy.php
+++ app/Policies/JobAdPolicy.php
@@ -5,6 +5,7 @@ namespace App\Policies;
 use App\Http\Requests\JobAd\StoreRequest;
 use App\Http\Requests\JobAd\UpdateRequest;
 use App\Models\JobAd;
+use Illuminate\Http\Request;
 
 class JobAdPolicy
 {
@@ -14,6 +15,20 @@ class JobAdPolicy
     }
 
     public function update(UpdateRequest $request, JobAd $jobAd): bool
+    {
+        $result = false;
+
+        $companies = $request->user()->companies;
+
+        foreach ($companies as $company) {
+            if ($jobAd->company == $company)
+                $request = true;
+        }
+
+        return ($request->user()->role == 'Employer') && ($result);
+    }
+
+    public function delete(Request $request, JobAd $jobAd): bool
     {
         $result = false;
 
