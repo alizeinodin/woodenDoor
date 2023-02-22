23ee4c0564f7472496a3f000fb167818378ec552
alizeinodin <alizeinodin79@gmail.com>
2023-02-22 16:21:32 +0330

    Add update method

--- app/Policies/JobAdPolicy.php
+++ app/Policies/JobAdPolicy.php
@@ -3,19 +3,27 @@
 namespace App\Policies;
 
 use App\Http\Requests\JobAd\StoreRequest;
+use App\Http\Requests\JobAd\UpdateRequest;
+use App\Models\JobAd;
 
 class JobAdPolicy
 {
-    /**
-     * Create a new policy instance.
-     */
-    public function __construct()
-    {
-        //
-    }
-
     public function create(StoreRequest $request): bool
     {
         return $request->user()->role == 'Employer';
+    }
+
+    public function update(UpdateRequest $request, JobAd $jobAd): bool
+    {
+        $result = false;
+
+        $companies = $request->user()->companies;
+
+        foreach ($companies as $company) {
+            if ($jobAd->company == $company)
+                $request = true;
+        }
+
+        return ($request->user()->role == 'Employer') && ($result);
     }
 }
