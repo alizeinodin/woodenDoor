a5d86bb8c59ce04566b4ae4dbd2415dbdacaecb4
alizeinodin <alizeinodin79@gmail.com>
2023-02-22 17:50:52 +0330

    Add foregin key for companies table

--- database/migrations/2023_02_15_110305_add_foreign_keys_to_companies_table.php
+++ database/migrations/2023_02_15_110305_add_foreign_keys_to_companies_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -15,6 +14,7 @@ return new class extends Migration
     {
         Schema::table('companies', function (Blueprint $table) {
             $table->foreign(['employer_id'], 'FK_EmployerID')->references(['user_id'])->on('employers')->onUpdate('CASCADE')->onDelete('CASCADE');
+            $table->foreign(['job_category_id'], 'FK_JobCategoryCompany')->references(['id'])->on('job_categories')->onUpdate('CASCADE')->onDelete('CASCADE');
         });
     }
 
