86b4eb9dcbb9b1e34605593dd998c0162e52c22b
alizeinodin <alizeinodin79@gmail.com>
2023-02-23 23:45:41 +0330

    Add UploadController

--- app/Http/Controllers/v1/UploadController.php
+++ app/Http/Controllers/v1/UploadController.php
@@ -3,9 +3,14 @@
 namespace App\Http\Controllers\v1;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\File;
-use Illuminate\Http\Request;
+use Illuminate\Http\UploadedFile;
 
 class UploadController extends Controller
 {
+    public const IMAGE_DIR = 'images';
+
+    public function storeImage(UploadedFile $file): bool|string
+    {
+        return $file->store(self::IMAGE_DIR);
+    }
 }
