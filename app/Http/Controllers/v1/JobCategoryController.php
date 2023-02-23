c3574763a4514fad934889689d4ee800878cbd19
alizeinodin <alizeinodin79@gmail.com>
2023-02-23 14:07:20 +0330

    Add store method in JobCategoryController

--- app/Http/Controllers/v1/JobCategoryController.php
+++ app/Http/Controllers/v1/JobCategoryController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers\v1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\JobCategory\StoreRequest;
 use App\Models\JobCategory;
+use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+use Illuminate\Contracts\Routing\ResponseFactory;
+use Illuminate\Http\Response;
+use Symfony\Component\HttpFoundation\Response as ResponseHttp;
 
 class JobCategoryController extends Controller
 {
@@ -27,6 +32,21 @@ class JobCategoryController extends Controller
         return $category->companies()->paginate(15);
     }
 
+    public function store(StoreRequest $request): Response|Application|ResponseFactory
+    {
+        $cleanData = $request->validated();
+
+        $jobCategory = new JobCategory();
+
+        $jobCategory->name = $cleanData['name'];
+
+        $response = [
+            'message' => 'Your job category added',
+            'object' => $jobCategory,
+        ];
+
+        return response($response, ResponseHttp::HTTP_CREATED);
+    }
 
 
 }
