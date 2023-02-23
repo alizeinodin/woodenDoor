92c0f859f39f55b05782979be7592516535e16d9
alizeinodin <alizeinodin79@gmail.com>
2023-02-23 15:03:42 +0330

    Add JobCategoryTest

---
+++ tests/Feature/JobCategoryTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class JobCategoryTest extends TestCase
+{
+    use WithFaker;
+}
