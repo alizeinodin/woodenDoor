bd5cc7b0db4968d0b2154c5d1c34560f306b1572
alizeinodin <alizeinodin79@gmail.com>
2023-02-23 23:36:03 +0330

    Add UploadController

---
+++ app/Http/Controllers/v1/UploadController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\v1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\File;
+use Illuminate\Http\Request;
+
+class UploadController extends Controller
+{
+}
