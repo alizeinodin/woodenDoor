b7eb9a96b929e354b06f67ecc7d6e66800d6caa7
alizeinodin <alizeinodin79@gmail.com>
2023-02-23 14:21:16 +0330

    Add seeder for job category

--- database/seeders/JobCategorySeeder.php
+++ database/seeders/JobCategorySeeder.php
@@ -15,5 +15,39 @@ class JobCategorySeeder extends Seeder
         JobCategory::create([
             'name' => 'پیشفرض',
         ]);
+
+        JobCategory::create([
+            'name' => 'دیجیتال مارکتینگ و سئو',
+        ]);
+
+        JobCategory::create([
+            'name' => 'توسعه نرم افزار و برنامه نویسی',
+        ]);
+
+        JobCategory::create([
+            'name' => 'تست نرم افزار',
+        ]);
+
+        JobCategory::create([
+            'name' => 'شبکه و دواپس',
+        ]);
+        JobCategory::create([
+            'name' => 'علوم داده و هوش مصنوعی',
+        ]);
+        JobCategory::create([
+            'name' => 'طراحی بازی',
+        ]);
+        JobCategory::create([
+            'name' => 'گرافیک',
+        ]);
+        JobCategory::create([
+            'name' => 'مدیریت بیمه',
+        ]);
+        JobCategory::create([
+            'name' => 'فروشگاه و رستوران',
+        ]);
+        JobCategory::create([
+            'name' => 'ترجمه و تولید محتوا',
+        ]);
     }
 }
