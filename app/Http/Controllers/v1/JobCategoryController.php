b1f74e89aad1be44a5e5134f13763069c216f046
alizeinodin <alizeinodin79@gmail.com>
2023-02-23 13:58:21 +0330

    Add index method

--- app/Http/Controllers/v1/JobCategoryController.php
+++ app/Http/Controllers/v1/JobCategoryController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers\v1;
 
 use App\Http\Controllers\Controller;
+use App\Models\JobCategory;
 
 class JobCategoryController extends Controller
 {
-    //
+    /**
+     * Display a listing of the
+     * job categories 15 pagination
+     */
+    public function index()
+    {
+        return JobCategory::paginate(15);
+    }
+
 }
