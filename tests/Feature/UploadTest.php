1ab57206002da174a75a3d58efecbf03bdd48a20
alizeinodin <alizeinodin79@gmail.com>
2023-02-24 01:11:14 +0330

    Debug test upload image

--- tests/Feature/UploadTest.php
+++ tests/Feature/UploadTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use App\Http\Controllers\v1\UploadController;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 use Tests\TestCase;
 
 class UploadTest extends TestCase
@@ -13,11 +14,13 @@ class UploadTest extends TestCase
 
     public function test_upload_image()
     {
+        Storage::fake('images');
+
         $file = UploadedFile::fake()->image('test.jpg');
 
         $uploader = new UploadController();
         $path = $uploader->storeImage($file);
 
-        $this->assertFileExists($path);
+        $this->assertFileExists("storage/app/$path");
     }
 }
