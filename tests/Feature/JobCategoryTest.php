c6b3bc9347e823ec1753979dfb711234dc4ccb46
alizeinodin <alizeinodin79@gmail.com>
2023-02-23 15:17:07 +0330

    Add test get all job categories

--- tests/Feature/JobCategoryTest.php
+++ tests/Feature/JobCategoryTest.php
@@ -2,11 +2,30 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\User;
 use Illuminate\Foundation\Testing\WithFaker;
+use Laravel\Sanctum\Sanctum;
 use Tests\TestCase;
 
 class JobCategoryTest extends TestCase
 {
     use WithFaker;
+
+    protected string $route_name = 'job_category';
+
+
+    public function test_authorize_for_sanctum_user()
+    {
+        $response = $this->getJson(route("api.$this->route_name.index"));
+        $response->assertStatus(401);
+    }
+
+    public function test_get_all_job_categories()
+    {
+        Sanctum::actingAs(
+            User::factory()->create()
+        );
+        $response = $this->getJson(route("api.$this->route_name.index"));
+        $response->assertOk();
+    }
 }
