9e846a78a04a00f1df85576bf0d0b8ecc20a3b59
alizeinodin <alizeinodin79@gmail.com>
2023-02-25 16:40:33 +0330

    Add store method

--- app/Http/Controllers/v1/Post/PostCategoryController.php
+++ app/Http/Controllers/v1/Post/PostCategoryController.php
@@ -3,7 +3,13 @@
 namespace App\Http\Controllers\v1\Post;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\PostCategory\StoreRequest;
 use App\Models\PostCategory;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\Routing\ResponseFactory;
+use Illuminate\Http\Response;
+use Illuminate\Support\Str;
+use Symfony\Component\HttpFoundation\Response as ResponseHttp;
 
 class PostCategoryController extends Controller
 {
@@ -14,5 +20,25 @@ class PostCategoryController extends Controller
     public function index()
     {
         return PostCategory::paginate(15);
+    }
+
+    public function store(StoreRequest $request): Response|Application|ResponseFactory
+    {
+        $cleanData = $request->validated();
+
+        $postCategory = new PostCategory();
+
+        $postCategory->title = $cleanData['title'];
+        $postCategory->description = $cleanData['description'];
+        $postCategory->link = $cleanData['link'] ?? Str::slug($cleanData['link']);
+
+        $postCategory->save();
+
+        $response = [
+            'message' => 'job category added',
+            'object' => $postCategory,
+        ];
+
+        return response($response, ResponseHttp::HTTP_CREATED);
     }
 }
