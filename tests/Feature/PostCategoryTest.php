c57e1c9de739a2c7cc11afd41a471894fd8f4bba
alizeinodin <alizeinodin79@gmail.com>
2023-02-28 12:47:09 +0330

    Add test_delete_post_category method

--- tests/Feature/PostCategoryTest.php
+++ tests/Feature/PostCategoryTest.php
@@ -56,4 +56,19 @@ class PostCategoryTest extends TestCase
         $postCategory = PostCategory::find($postCategory->id);
         $this->assertEquals('title2', $postCategory->title);
     }
+
+    public function test_delete_post_category()
+    {
+        $user = User::factory()->create();
+
+        Sanctum::actingAs($user);
+
+        $postCategory = new PostCategory();
+        $postCategory->title = $this->faker->name;
+        $postCategory->link = $this->faker->url;
+        $postCategory->save();
+
+        $response = $this->deleteJson(route("api.$this->route_name.destroy", ['post_category' => $postCategory]));
+        $response->assertStatus(204);
+    }
 }
