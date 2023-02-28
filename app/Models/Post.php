019fb9c704f7d977a683358e04bb5ef72698fa11
alizeinodin <alizeinodin79@gmail.com>
2023-02-28 13:28:43 +0330

    Add one to many relationship between post and post category

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
@@ -21,4 +22,9 @@ class Post extends Model
         'uri',
         'index_image',
     ];
+
+    public function post_category(): BelongsTo
+    {
+        return $this->belongsTo(PostCategory::class, 'category_id', 'id');
+    }
 }
