72effec1984d60f2a0f8cd050b13af29c0048785
alizeinodin <alizeinodin79@gmail.com>
2023-02-28 17:18:47 +0330

    Change length of title and uri

--- database/migrations/2023_02_15_110304_create_posts_table.php
+++ database/migrations/2023_02_15_110304_create_posts_table.php
@@ -16,13 +16,13 @@ return new class extends Migration {
             $table->comment('');
             $table->integer('id', true);
             $table->integer('category_id')->default(1)->index('category_id');
-            $table->string('title', 50);
+            $table->string('title', 100)->unique();
             $table->string('description', 76)->nullable();
             $table->longText('content')->nullable();
             $table->enum('post_status', ['0', '1', '2', '3', '4'])->default('3')->comment('#0 : pending | #1 : ejected | #2 : published | #3 : draft | #4 : deleted');
             $table->boolean('comment_status')->default(true)->comment('#true: open comment | #false: close comment');
             $table->integer('score')->default(0);
-            $table->string('uri', 50);
+            $table->string('uri', 150)->unique();
             $table->integer('index_image')->nullable()->index('IndexImage')->comment('this column refer to media');
             $table->timestamps();
             $table->integer('author_id')->index('FK_PAuthorId');
