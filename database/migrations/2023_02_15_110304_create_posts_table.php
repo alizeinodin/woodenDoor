beca604132cd46fff42c648d37c3499f085cc5a0
alizeinodin <alizeinodin79@gmail.com>
2023-02-28 13:10:29 +0330

    Add timestamps to posts table

--- database/migrations/2023_02_15_110304_create_posts_table.php
+++ database/migrations/2023_02_15_110304_create_posts_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -25,8 +24,7 @@ return new class extends Migration
             $table->integer('score')->default(0);
             $table->string('uri', 50);
             $table->integer('index_image')->nullable()->index('IndexImage')->comment('this column refer to media');
-            $table->timestamp('created_at')->useCurrent();
-            $table->dateTime('updated_at')->useCurrent();
+            $table->timestamps();
             $table->integer('author_id')->index('FK_PAuthorId');
         });
     }
