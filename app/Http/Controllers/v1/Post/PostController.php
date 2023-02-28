a1eb21cf8cec16ea765cbd17b3aff2b8ecc7c455
alizeinodin <alizeinodin79@gmail.com>
2023-02-28 17:06:08 +0330

    Add index method

--- app/Http/Controllers/v1/Post/PostController.php
+++ app/Http/Controllers/v1/Post/PostController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers\v1\Post;
 
 use App\Http\Controllers\Controller;
+use App\Models\Post;
 
 class PostController extends Controller
 {
-
+    /**
+     * Display a listing of the
+     * post 15 pagination
+     */
+    public function index()
+    {
+        return Post::paginate(15);
+    }
 }
