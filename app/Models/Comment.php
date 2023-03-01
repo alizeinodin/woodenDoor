a9bd18ad994b4efb117140a1c9475f74d59a6538
alizeinodin <alizeinodin79@gmail.com>
2023-03-01 18:35:00 +0330

    Add one to many relationship for reply comment

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Comment extends Model
 {
@@ -20,5 +21,25 @@ class Comment extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    /**
+     * parent of a comment
+     *
+     * @return BelongsTo
+     */
+    public function parent(): BelongsTo
+    {
+        return $this->belongsTo(Comment::class, 'comment_id', 'id')->with('parent');
+    }
+
+    /**
+     * children of a comment
+     *
+     * @return HasMany
+     */
+    public function children(): HasMany
+    {
+        return $this->hasMany(Comment::class, 'comment_id', 'id')->with('children');
     }
 }
