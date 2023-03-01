2e0b55e125a56d8010e2f29b65565af3b03aeeb4
alizeinodin <alizeinodin79@gmail.com>
2023-03-01 09:52:00 +0330

    Add posts routes

---
+++ routes/Api/post.php
@@ -0,0 +1,21 @@
+<?php
+
+
+use App\Http\Controllers\v1\Post\PostController;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::name('post.')->group(function () {
+        Route::controller(PostController::class)->group(function () {
+
+        });
+    });
+    Route::resource('post', PostController::class)
+        ->only([
+            'index',
+            'show',
+            'store',
+            'update',
+            'destroy'
+        ]);
+});
