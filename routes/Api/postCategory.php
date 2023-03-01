6c26dfea5ab00d48d381633901e19f627a68ae6e
alizeinodin <alizeinodin79@gmail.com>
2023-03-01 16:24:08 +0330

    Add show_posts route

--- routes/Api/postCategory.php
+++ routes/Api/postCategory.php
@@ -7,7 +7,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->group(function () {
     Route::name('post_category.')->group(function () {
         Route::controller(PostCategoryController::class)->group(function () {
-
+            Route::get('/{postCategory}/posts', 'show_posts')
+                ->name('show_posts');
         });
     });
     Route::resource('post_category', PostCategoryController::class)
