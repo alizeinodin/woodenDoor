b62a94f1c7444d1828ce802ad2f9337c53275ccc
alizeinodin <alizeinodin79@gmail.com>
2023-03-01 09:58:16 +0330

    Add test auhorize for sanctum user method

--- tests/Feature/PostTest.php
+++ tests/Feature/PostTest.php
@@ -2,8 +2,20 @@
 
 namespace Tests\Feature;
 
+use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class PostTest extends TestCase
 {
+    use WithFaker;
+
+    protected string $route_name = 'post';
+
+
+    public function test_authorize_for_sanctum_user()
+    {
+        $response = $this->getJson(route("api.$this->route_name.index"));
+        $response->assertStatus(401);
+    }
+
 }
