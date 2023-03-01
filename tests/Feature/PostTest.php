515d564b03ada2ed1971fe57d119691f0e4ebb5b
alizeinodin <alizeinodin79@gmail.com>
2023-03-01 09:59:18 +0330

    Add test get all posts method

--- tests/Feature/PostTest.php
+++ tests/Feature/PostTest.php
@@ -2,7 +2,9 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\WithFaker;
+use Laravel\Sanctum\Sanctum;
 use Tests\TestCase;
 
 class PostTest extends TestCase
@@ -16,6 +18,15 @@ class PostTest extends TestCase
     {
         $response = $this->getJson(route("api.$this->route_name.index"));
         $response->assertStatus(401);
+    }
+
+    public function test_get_all_posts()
+    {
+        Sanctum::actingAs(
+            User::factory()->create()
+        );
+        $response = $this->getJson(route("api.$this->route_name.index"));
+        $response->assertOk();
     }
 
 }
