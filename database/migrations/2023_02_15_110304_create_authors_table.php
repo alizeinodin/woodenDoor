3902f5675772fd3a1cd8c34ae1316b5d9fbaf5d6
alizeinodin <alizeinodin79@gmail.com>
2023-03-01 11:23:04 +0330

    Add timestamps to authors table

--- database/migrations/2023_02_15_110304_create_authors_table.php
+++ database/migrations/2023_02_15_110304_create_authors_table.php
@@ -19,6 +19,7 @@ return new class extends Migration {
             $table->integer('score')->default(0);
             $table->text('about')->nullable();
             $table->integer('user_id')->index('FK_AEmployeeId');
+            $table->timestamps();
         });
     }
 
