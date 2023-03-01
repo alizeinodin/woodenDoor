51d30bc63360352746fae71c450406f294067701
alizeinodin <alizeinodin79@gmail.com>
2023-03-01 18:20:40 +0330

    Add one to many relationship between comment and post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Post extends Model
 {
@@ -31,5 +32,10 @@ class Post extends Model
     public function author(): BelongsTo
     {
         return $this->belongsTo(Author::class);
+    }
+
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class);
     }
 }
