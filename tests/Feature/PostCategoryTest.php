91d99249ef9ef26dac2650be2397052efe7b924e
alizeinodin <alizeinodin79@gmail.com>
2023-03-01 16:29:06 +0330

    Add test show all posts of category method

--- tests/Feature/PostCategoryTest.php
+++ tests/Feature/PostCategoryTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature;
 
+use App\Models\Author;
+use App\Models\Post;
 use App\Models\PostCategory;
 use App\Models\User;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -70,5 +72,33 @@ class PostCategoryTest extends TestCase
 
         $response = $this->deleteJson(route("api.$this->route_name.destroy", ['post_category' => $postCategory]));
         $response->assertStatus(204);
+    }
+
+    public function test_show_all_posts_of_category()
+    {
+
+        $user = User::factory()->create();
+
+        Sanctum::actingAs($user);
+
+        $postCategory = new PostCategory();
+        $postCategory->title = $this->faker->name;
+        $postCategory->link = $this->faker->url;
+        $postCategory->save();
+
+        $author = new Author();
+        $user->author()->save($author);
+
+        $post = new Post();
+        $post->title = $this->faker()->name;
+        $post->description = $this->faker()->name;
+        $post->content = $this->faker()->text;
+        $post->uri = $this->faker()->url;
+        $post->category_id = $postCategory->id;
+
+        $author->posts()->save($post);
+
+        $response = $this->getJson(route("api.$this->route_name.show_posts", ['postCategory' => $postCategory]));
+        $response->assertStatus(200);
     }
 }
