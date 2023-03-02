37f89d68a65189af13d2065c50284c651667fa9c
alizeinodin <alizeinodin79@gmail.com>
2023-03-02 21:21:13 +0330

    Add get comments method

--- app/Http/Controllers/v1/CommentController.php
+++ app/Http/Controllers/v1/CommentController.php
@@ -4,13 +4,19 @@ namespace App\Http\Controllers\v1;
 
 use App\Http\Controllers\Controller;
 use App\Models\Comment;
+use App\Models\Post;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
     public function index(): Collection
     {
         return Comment::all();
+    }
+
+    public function get_comments(Post $post): LengthAwarePaginator
+    {
+        return $post->comments()->paginate(15);
     }
 }
