b488ecd8e3c29a5411084b49862aee3ba7f23ae7
alizeinodin <alizeinodin79@gmail.com>
2023-03-02 21:23:08 +0330

    Add show method

--- app/Http/Controllers/v1/CommentController.php
+++ app/Http/Controllers/v1/CommentController.php
@@ -5,8 +5,12 @@ namespace App\Http\Controllers\v1;
 use App\Http\Controllers\Controller;
 use App\Models\Comment;
 use App\Models\Post;
+use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+use Illuminate\Contracts\Routing\ResponseFactory;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Http\Response;
+use Symfony\Component\HttpFoundation\Response as ResponseHttp;
 
 class CommentController extends Controller
 {
@@ -18,5 +22,10 @@ class CommentController extends Controller
     public function get_comments(Post $post): LengthAwarePaginator
     {
         return $post->comments()->paginate(15);
+    }
+
+    public function show(Comment $comment): Application|ResponseFactory|Response
+    {
+        return response($comment, ResponseHttp::HTTP_OK);
     }
 }
