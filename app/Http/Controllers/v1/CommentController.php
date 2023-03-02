8eec9c235989aebd7839236010ff27b9091d4f11
alizeinodin <alizeinodin79@gmail.com>
2023-03-02 21:18:51 +0330

    Add index method

--- app/Http/Controllers/v1/CommentController.php
+++ app/Http/Controllers/v1/CommentController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers\v1;
 
 use App\Http\Controllers\Controller;
+use App\Models\Comment;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
+    public function index(): Collection
+    {
+        return Comment::all();
+    }
 }
