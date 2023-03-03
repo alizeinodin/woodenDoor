c2dee425e6a541f0b8ca2f8e1a8275eed24678bc
alizeinodin <alizeinodin79@gmail.com>
2023-03-03 10:14:04 +0330

    Add store method

--- app/Http/Controllers/v1/CommentController.php
+++ app/Http/Controllers/v1/CommentController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\v1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Comment\StoreRequest;
 use App\Models\Comment;
 use App\Models\Post;
 use Illuminate\Contracts\Foundation\Application;
@@ -27,5 +28,25 @@ class CommentController extends Controller
     public function show(Comment $comment): Application|ResponseFactory|Response
     {
         return response($comment, ResponseHttp::HTTP_OK);
+    }
+
+    public function store(StoreRequest $request, Post $post): Response|Application|ResponseFactory
+    {
+        $cleanData = $request->validated();
+
+        $comment = new Comment();
+
+        $comment->content = $cleanData['content'];
+        $comment->comment_id = $cleanData['comment_id'];
+        $comment->post_id = $post->id;
+
+        $request->user()->comments()->save($comment);
+
+        $response = [
+            'message' => 'Comment added',
+            'object' => $comment,
+        ];
+
+        return response($response, ResponseHttp::HTTP_CREATED);
     }
 }
