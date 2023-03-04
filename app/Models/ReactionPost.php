1f06b4c4f932ff2f73f35db1b291d16ad2a72693
alizeinodin <alizeinodin79@gmail.com>
2023-03-04 16:13:03 +0330

    Add fillable and casts to ReactionPost model

--- app/Models/ReactionPost.php
+++ app/Models/ReactionPost.php
@@ -2,10 +2,21 @@
 
 namespace App\Models;
 
+use App\Enum\Reaction;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ReactionPost extends Model
 {
+    protected $table = 'reaction_posts';
+
     use HasFactory;
+
+    protected $fillable = [
+        'react',
+    ];
+
+    protected $casts = [
+        'react' => Reaction::class,
+    ];
 }
