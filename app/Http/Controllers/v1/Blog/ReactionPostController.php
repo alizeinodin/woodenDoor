f7b61d217c1ee78acb4bb0a70a2a8779eca94458
alizeinodin <alizeinodin79@gmail.com>
2023-03-04 16:37:41 +0330

    Add index method

--- app/Http/Controllers/v1/Blog/ReactionPostController.php
+++ app/Http/Controllers/v1/Blog/ReactionPostController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers\v1\Blog;
 
 use App\Http\Controllers\Controller;
+use App\Models\ReactionPost;
 
 class ReactionPostController extends Controller
 {
-
+    public function index()
+    {
+        return ReactionPost::paginate(15);
+    }
 }
