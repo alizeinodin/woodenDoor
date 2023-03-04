4c1e8a614952c702608428445e5062c01a57763c
alizeinodin <alizeinodin79@gmail.com>
2023-03-04 18:09:34 +0330

    Add test authorize for sanctum users method

--- tests/Feature/ReactionTest.php
+++ tests/Feature/ReactionTest.php
@@ -2,11 +2,18 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class ReactionTest extends TestCase
 {
+    use WithFaker;
 
+    protected string $route_name = 'reaction';
+
+    public function test_authorize_for_sanctum_user()
+    {
+        $response = $this->getJson(route("api.$this->route_name.index"));
+        $response->assertStatus(401);
+    }
 }
