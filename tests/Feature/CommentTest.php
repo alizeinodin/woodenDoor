eb664baeee85715d7536fafa31f319b92cd1c43a
alizeinodin <alizeinodin79@gmail.com>
2023-03-04 11:10:22 +0330

    Add test store comment

--- tests/Feature/CommentTest.php
+++ tests/Feature/CommentTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature;
 
+use App\Models\Author;
+use App\Models\Post;
 use App\Models\User;
 use Illuminate\Foundation\Testing\WithFaker;
 use Laravel\Sanctum\Sanctum;
@@ -11,7 +13,7 @@ class CommentTest extends TestCase
 {
     use WithFaker;
 
-    protected string $route_name = 'post';
+    protected string $route_name = 'comment';
 
 
     public function test_authorize_for_sanctum_user()
@@ -27,5 +29,34 @@ class CommentTest extends TestCase
         );
         $response = $this->getJson(route("api.$this->route_name.index"));
         $response->assertOk();
+    }
+
+    public function test_store_comment()
+    {
+        $user = User::factory()->create();
+
+        $author = new Author();
+        $user->author()->save($author);
+
+        Sanctum::actingAs($user);
+
+        $post = new Post();
+
+        $post->title = $this->faker()->name;
+        $post->description = $this->faker()->name;
+        $post->content = $this->faker()->text;
+        $post->uri = $this->faker()->url;
+
+        $author->posts()->save($post);
+
+        $request = [
+            'title' => $this->faker()->name,
+            'description' => $this->faker()->name,
+            'content' => $this->faker()->text,
+            'post_id' => $post->id,
+        ];
+
+        $response = $this->postJson(route("api.$this->route_name.store", ['post' => $post]), $request);
+        $response->assertCreated();
     }
 }
