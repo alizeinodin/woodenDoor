3713bbd0058c9e2fb63d337b9e30545a8014aed7
alizeinodin <alizeinodin79@gmail.com>
2023-03-06 10:34:39 +0330

    Add storePost to RouteServiceProvider

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -41,6 +41,7 @@ class RouteServiceProvider extends ServiceProvider
                     Route::namespace($this->namespace)->group(base_path('routes/Api/post.php'));
                     Route::namespace($this->namespace)->group(base_path('routes/Api/comment.php'));
                     Route::namespace($this->namespace)->group(base_path('routes/Api/reaction.php'));
+                    Route::namespace($this->namespace)->group(base_path('routes/Api/storePost.php'));
                 });
 
             Route::middleware('web')
