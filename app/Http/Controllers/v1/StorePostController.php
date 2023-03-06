b240b56a0f8eea2e192267a2a95b4400b432e00f
alizeinodin <alizeinodin79@gmail.com>
2023-03-06 10:28:04 +0330

    Add storePost method

--- app/Http/Controllers/v1/StorePostController.php
+++ app/Http/Controllers/v1/StorePostController.php
@@ -3,8 +3,23 @@
 namespace App\Http\Controllers\v1;
 
 use App\Http\Controllers\Controller;
+use App\Models\Post;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\Routing\ResponseFactory;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Symfony\Component\HttpFoundation\Response as ResponseHttp;
 
 class StorePostController extends Controller
 {
+    public function storePost(Request $request, Post $post): Response|Application|ResponseFactory
+    {
+        $post->usersStored()->attach($request->user);
 
+        $response = [
+            'message' => 'The post added to your account',
+        ];
+
+        return response($response, ResponseHttp::HTTP_OK);
+    }
 }
