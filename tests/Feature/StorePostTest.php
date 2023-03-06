e717cddfb6140be6b9862f83114ce00f1f244da7
alizeinodin <alizeinodin79@gmail.com>
2023-03-06 10:49:22 +0330

    Add test authorize for sanctum user method

--- tests/Feature/StorePostTest.php
+++ tests/Feature/StorePostTest.php
@@ -2,8 +2,19 @@
 
 namespace Tests\Feature;
 
+use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class StorePostTest extends TestCase
 {
+    use WithFaker;
+
+    protected string $route_name = 'store_post';
+
+    public function test_authorize_for_sanctum_user()
+    {
+        $response = $this->postJson(route("api.$this->route_name.store", ['post' => 1]));
+        $response->assertStatus(401);
+    }
+
 }
