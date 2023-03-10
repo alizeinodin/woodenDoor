c349d0f550143a89f5700367b688b60509dae2af
alizeinodin <alizeinodin79@gmail.com>
2023-03-11 01:23:54 +0330

    Rename table

--- database/migrations/2023_02_15_110304_create_skills_of_job_ads_table.php
+++ database/migrations/2023_02_15_110304_create_skills_of_job_ads_table.php
@@ -27,6 +27,6 @@ return new class extends Migration {
      */
     public function down()
     {
-        Schema::dropIfExists('job_properties');
+        Schema::dropIfExists('skills_of_job_ads');
     }
 };
--- database/migrations/2023_02_15_110305_add_foreign_keys_to_skills_of_job_ads_table.php
+++ database/migrations/2023_02_15_110305_add_foreign_keys_to_skills_of_job_ads_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -13,7 +12,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::table('job_properties', function (Blueprint $table) {
+        Schema::table('skills_of_job_ads', function (Blueprint $table) {
             $table->foreign(['job_ads_property_id'], 'FK_JobPropertyTwo')->references(['id'])->on('job_ads_properties')->onUpdate('CASCADE')->onDelete('CASCADE');
             $table->foreign(['job_ads_id'], 'FK_JobPropertyOne')->references(['id'])->on('job_ads')->onUpdate('CASCADE')->onDelete('CASCADE');
         });
@@ -26,7 +25,7 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::table('job_properties', function (Blueprint $table) {
+        Schema::table('skills_of_job_ads', function (Blueprint $table) {
             $table->dropForeign('FK_JobPropertyTwo');
             $table->dropForeign('FK_JobPropertyOne');
         });
