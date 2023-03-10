f1a5bc3369caee9ec9b32fa9f900f9600cc7cc18
alizeinodin <alizeinodin79@gmail.com>
2023-03-11 02:04:08 +0330

    Update and develop UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,7 +3,6 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -22,6 +21,8 @@ class UserFactory extends Factory
             'first_name' => fake()->name(),
             'last_name' => fake()->lastName(),
             'email' => fake()->unique()->safeEmail(),
+            'phone' => fake()->unique()->phoneNumber(),
+            'birth_year' => fake()->unique()->date(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'sex' => true,
         ];
