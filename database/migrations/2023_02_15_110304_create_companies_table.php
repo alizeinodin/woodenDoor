8a6f0e936b071de8e391c2a1dc3bacff92b1d6f9
alizeinodin <alizeinodin79@gmail.com>
2023-03-11 00:32:50 +0330

    Add isValid in companies table

--- database/migrations/2023_02_15_110304_create_companies_table.php
+++ database/migrations/2023_02_15_110304_create_companies_table.php
@@ -24,6 +24,7 @@ return new class extends Migration {
             $table->enum('number_of_staff', ['0', '1', '2', '3', '4', '5'])->nullable()->comment('#0: under 10 employer | #1: 10 to 20 employer | #2: 20 to 50 employer | #3: 50 to 100 employer | #4: 4: 100 to 500 employer | #5: more than 500');
             $table->text('about_company')->nullable();
             $table->string('nick_name', 50)->unique('nick_name');
+            $table->boolean('isValid')->default(false);
             $table->integer('employer_id')->index('FK_EmployerID');
             $table->integer('job_category_id')->index('FK_JobCategoryCompany')->default(1);
             $table->timestamps();
