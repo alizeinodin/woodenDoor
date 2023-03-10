d81e1e90cb9fdd06743f339524963d45b308e01e
alizeinodin <alizeinodin79@gmail.com>
2023-03-11 00:46:51 +0330

    Add link for JobCategorySeeder

--- database/seeders/JobCategorySeeder.php
+++ database/seeders/JobCategorySeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\JobCategory;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class JobCategorySeeder extends Seeder
 {
@@ -14,40 +15,52 @@ class JobCategorySeeder extends Seeder
     {
         JobCategory::create([
             'title' => 'پیشفرض',
+            'link' => Str::slug('پیشفرض'),
         ]);
 
         JobCategory::create([
             'title' => 'دیجیتال مارکتینگ و سئو',
+            'link' => Str::slug('دیجیتال مارکتینگ و سئو'),
+
         ]);
 
         JobCategory::create([
             'title' => 'توسعه نرم افزار و برنامه نویسی',
+            'link' => Str::slug('توسعه نرم افزار و برنامه نویسی'),
         ]);
 
         JobCategory::create([
             'title' => 'تست نرم افزار',
+            'link' => Str::slug('تست نرم افزار'),
         ]);
 
         JobCategory::create([
             'title' => 'شبکه و دواپس',
+            'link' => Str::slug('شبکه و دواپس'),
         ]);
         JobCategory::create([
             'title' => 'علوم داده و هوش مصنوعی',
+            'link' => Str::slug('علوم داده و هوش مصنوعی'),
         ]);
         JobCategory::create([
             'title' => 'طراحی بازی',
+            'link' => Str::slug('طراحی بازی'),
         ]);
         JobCategory::create([
             'title' => 'گرافیک',
+            'link' => Str::slug('گرافیک'),
         ]);
         JobCategory::create([
             'title' => 'مدیریت بیمه',
+            'link' => Str::slug('مدیریت بیمه'),
         ]);
         JobCategory::create([
             'title' => 'فروشگاه و رستوران',
+            'link' => Str::slug('فروشگاه و رستوران'),
         ]);
         JobCategory::create([
             'title' => 'ترجمه و تولید محتوا',
+            'link' => Str::slug('ترجمه و تولید محتوا'),
         ]);
     }
 }
