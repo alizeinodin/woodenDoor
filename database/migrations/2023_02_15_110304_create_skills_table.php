c426ea4ac7d16e9ca0dc1f24661c6353728b42f5
alizeinodin <alizeinodin79@gmail.com>
2023-03-11 01:31:59 +0330

    Update skills table

--- database/migrations/2023_02_15_110304_create_skills_table.php
+++ database/migrations/2023_02_15_110304_create_skills_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -15,12 +14,10 @@ return new class extends Migration
     {
         Schema::create('skills', function (Blueprint $table) {
             $table->comment('');
-            $table->integer('id');
-            $table->integer('employee_id')->index('FK_SKEmpId');
+            $table->integer('id')->primary();
             $table->string('name', 50);
             $table->integer('score')->default(0);
-
-            $table->primary(['id', 'employee_id']);
+            $table->timestamps();
         });
     }
 
