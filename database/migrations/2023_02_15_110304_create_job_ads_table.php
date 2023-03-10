6a1c0692f4f81a9433fe8a7ad59dc82fe95a51ca
alizeinodin <alizeinodin79@gmail.com>
2023-03-11 01:03:25 +0330

    Add max salary column to job_ads table

--- database/migrations/2023_02_15_110304_create_job_ads_table.php
+++ database/migrations/2023_02_15_110304_create_job_ads_table.php
@@ -21,6 +21,7 @@ return new class extends Migration {
             $table->string('province', 80);
             $table->enum('type_of_cooperation', ['0', '1', '2', '3'])->comment('#0: part time | #1: full time | #2: telecommuting | #3: project');
             $table->decimal('min_salary', 10, 0)->nullable();
+            $table->decimal('max_salary', 10, 0)->nullable();
             $table->text('description');
             $table->enum('work_experience', ['0', '1', '2', '3', '4', '5'])->nullable()->comment('#0: less than 6 month | #1: less than 1 year | #2: 1 to 2 year | #3: 2 to 3 years | #4: 3 to 5 years | #5: more than 5 years');
             $table->enum('min_education_degree', ['0', '1', '2', '3', '4', '5'])->nullable()->comment('#0: under diploma | #1: diploma | #2: bachelors degree | #3: masters degree | #4: doctorate | #5: postdoctoral');
