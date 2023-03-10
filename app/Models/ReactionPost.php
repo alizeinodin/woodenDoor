6003edc78b66d1a10b3ff7ee5d17016efc8e0745
alizeinodin <alizeinodin79@gmail.com>
2023-03-10 14:51:06 +0330

    Delete additional items from ReactionPost model

--- app/Models/ReactionPost.php
+++ app/Models/ReactionPost.php
@@ -9,8 +9,6 @@ use Illuminate\Database\Eloquent\Model;
 class ReactionPost extends Model
 {
     protected $table = 'reaction_posts';
-    protected $primaryKey = 'user_id,post_id';
-    public $incrementing = false;
 
     use HasFactory;
 
